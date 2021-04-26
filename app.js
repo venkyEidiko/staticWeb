cdf133dd2ed5bb73c5c26772247dde69cbffe664
venkata krishna <venkatakrishna9397@gmail.com>
2021-04-26 13:35:03 +0530

    static web

---
+++ app.js
@@ -0,0 +1,17 @@
+const express  = require('express');
+const app = express();
+const path = require('path');
+
+
+
+app.use(express.static(path.join(__dirname,'./public')));
+
+
+
+app.listen(1234,()=>{
+    console.log("server started at 1234")
+}
+
+)
+
+
